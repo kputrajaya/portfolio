84352c25b166f9d676e881a49c6645ed1dc4f30b
kiloev <freakperzon@yahoo.com>
2019-03-06 18:13:12 +0700

    Group skillsets

--- src/js/site.js
+++ src/js/site.js
@@ -1,27 +1,27 @@
 (document => {
 
-  const stripText = text => text.toLowerCase().replace(/[\s\.\-]/g, '')
+  const stripText = text => text.toLowerCase().replace(/[\s\.\-]/g, '');
 
   const findSkill = _ => {
-    const input = document.getElementById('findSkill')
-    const tags = [].slice.call(document.querySelectorAll('.tag > li'))
-    const map = tags.map(tag => stripText(`${tag.innerHTML}/${tag.dataset.alt || ''}`))
-    let timeout
+    const input = document.getElementById('findSkill');
+    const tags = [].slice.call(document.querySelectorAll('.tag > li'));
+    const map = tags.map(tag => stripText(`${tag.innerHTML}/${tag.dataset.alt || ''}`));
+    let timeout;
 
     const onChange = _ => {
-      clearTimeout(timeout)
+      clearTimeout(timeout);
       timeout = setTimeout(_ => {
-        const query = stripText(input.value)
+        const query = stripText(input.value);
         map.forEach((text, i) => {
-          tags[i].className = text.indexOf(query) !== -1 ? null : 'fade'
-        })
-      }, 125)
-    }
+          tags[i].className = text.indexOf(query) !== -1 ? null : 'fade';
+        });
+      }, 125);
+    };
 
-    input.addEventListener('change', onChange)
-    input.addEventListener('keyup', onChange)
+    input.addEventListener('change', onChange);
+    input.addEventListener('keyup', onChange);
   }
 
-  findSkill()
+  findSkill();
 
-})(document)
+})(document);
