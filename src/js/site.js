f597544abdd01455019266867710d71a6d6d10f1
kiloev <freakperzon@yahoo.com>
2019-04-09 08:29:35 +0700

    Revamp and blog skeleton

--- src/js/site.js
+++ src/js/site.js
@@ -1,16 +1,16 @@
-(document => {
+((document) => {
 
   const stripText = text => text.toLowerCase().replace(/[\s\.\-]/g, '');
 
-  const findSkill = _ => {
+  const findSkill = () => {
     const input = document.getElementById('findSkill');
     const tags = [].slice.call(document.querySelectorAll('.tag > li'));
     const map = tags.map(tag => stripText(`${tag.innerHTML}/${tag.dataset.alt || ''}`));
     let timeout;
 
-    const onChange = _ => {
+    const onChange = () => {
       clearTimeout(timeout);
-      timeout = setTimeout(_ => {
+      timeout = setTimeout(() => {
         const query = stripText(input.value);
         map.forEach((text, i) => {
           tags[i].className = text.indexOf(query) !== -1 ? null : 'fade';
