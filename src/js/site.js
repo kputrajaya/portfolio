ce2444943ff77393005367ce2c3090ce36350c75
kiloev <freakperzon@yahoo.com>
2019-10-05 12:24:58 +0700

    JS refactor

--- src/js/site.js
+++ src/js/site.js
@@ -5,22 +5,22 @@
   // Initializations
   const findSkill = () => {
     const input = document.getElementById('findSkill');
-    const tags = [].slice.call(document.querySelectorAll('.tag > li'));
-    const map = tags.map(tag => stripText(`${tag.innerHTML}/${tag.dataset.alt || ''}`));
+    const tags = Array.from(document.querySelectorAll('.tag > li'));
+    const texts = tags.map(tag => stripText(`${tag.innerHTML}/${tag.dataset.alt || ''}`));
     let timeout;
 
     const onEvent = () => {
       clearTimeout(timeout);
       timeout = setTimeout(() => {
         const query = stripText(input.value);
-        map.forEach((text, i) => {
+        texts.forEach((text, i) => {
           tags[i].className = text.indexOf(query) !== -1 ? null : 'fade';
         });
       }, 150);
     };
 
-    input.addEventListener('change', onEvent);
-    input.addEventListener('keyup', onEvent);
+    input.onblur = onEvent;
+    input.onkeyup = onEvent;
   };
 
   // Run
