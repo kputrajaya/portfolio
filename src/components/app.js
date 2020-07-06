bf5665dc1e0fbe79832738c5f98c9ef369e5777b
kiloev <freakperzon@yahoo.com>
2020-07-06 23:02:13 +0700

    Load Google Fonts in stylesheet

--- src/components/app.js
+++ src/components/app.js
@@ -7,8 +7,6 @@ import Home from '../routes/home';
 const App = () => {
 	return (
 		<div id="app">
-			<link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;900&family=Tajawal:wght@500&display=swap" rel="stylesheet" />
-
 			<Router>
 				<Home path="/" />
 			</Router>
