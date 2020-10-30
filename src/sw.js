2c1a104edf8c1f2b1a5a3fab6133669f67349570
kiloev <freakperzon@yahoo.com>
2020-10-30 11:56:42 +0700

    Service worker

--- src/sw.js
+++ src/sw.js
@@ -2,18 +2,18 @@ import { getFiles, setupPrecaching, setupRouting } from 'preact-cli/sw';
 import { registerRoute } from 'workbox-routing';
 import { StaleWhileRevalidate } from 'workbox-strategies';
 
+// Runtime cache
+registerRoute(
+  ({ url: { hostname } }) => (
+    hostname === 'fonts.googleapis.com' ||
+    hostname === 'fonts.gstatic.com'
+  ),
+  new StaleWhileRevalidate()
+);
+
 setupRouting();
 
 // Precache
 const precacheUrls = getFiles();
 precacheUrls.push({url: '/favicon.ico', revision: null});
 setupPrecaching(precacheUrls);
-
-// Runtime cache
-registerRoute(
-  ({ url }) => (
-    url.hostname === 'fonts.googleapis.com' ||
-    url.hostname === 'fonts.gstatic.com'
-  ),
-  new StaleWhileRevalidate()
-);
